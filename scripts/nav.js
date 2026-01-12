3c0a03e5514fd97e724a05ba0b24173c9f764425
Adi-Bak <21abhakri@gmail.com>
2026-01-12 15:06:42 +0000

    cloud things

--- scripts/nav.js
+++ scripts/nav.js
@@ -16,6 +16,12 @@ function is_this_active(page_name){
     let found= url.search(page_name.toLowerCase());
     
 
+    if(url =="http://127.0.0.1:5500/" || url =="http://127.0.0.1:5500/index.html"){
+        if(page_name =="HOME"){
+            return true;
+
+        }
+    }
 
     if(found>0){
         return true;
